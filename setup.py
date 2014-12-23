332d6ecd1e69a23854f24fbc8735f6ea9486c97e
Adam Kaufman <akaufman@veracode.com>
2014-12-23 12:27:49 -0500

    added nominal test and lots of setup improvements

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ setup(
     author='Adam Kaufman',
     author_email='kaufman.blue@gmail.com',
     url='https://github.com/ajk8/fencepy',
+    license='GNU GPLv3',
     packages=['fencepy'],
-    entry_points={'console_scripts': ['fencepy = fencepy:fence']}
+    entry_points={'console_scripts': ['fencepy = fencepy:fence']},
+    test_suite='tests',
+    install_requires=[
+        'virtualenv>=1.11'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Software Development'
+    ]
 )
---
+++ tests/test_fencepy.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class TestFencepy(TestCase):
+    def test_fencepy_import(self):
+        import fencepy
